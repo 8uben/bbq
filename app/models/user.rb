7f1492993f5ef6382e0fd5b1ba407fd0da958df7
Vladislav Fedyaev <vladislav.fdv@mail.ru>
2021-12-23 21:57:49 +0300

    comments & subscriptions

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,8 +5,26 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   # Юзер может создавать много событий
   has_many :events
+  has_many :comments
+  has_many :subscriptions
+
+    # Для конкретного event_id один юзер может подписаться только один раз (если юзер задан)
+  validates :user, uniqueness: {scope: :event_id}, if: -> { user.present? }
+
+  # Или один email может использоваться только один раз (если анонимная подписка)
+  validates :user_email, uniqueness: {scope: :event_id}, unless: -> { user.present? }
 
   before_validation :set_name, on: :create
+
+  after_commit :link_subscriptions, on: :create
+
+  def user_name
+    if user.present?
+      user.name
+    else
+      super
+    end
+  end
 
   private
 
@@ -14,4 +32,8 @@ class User < ApplicationRecord
     self.name = "Товарисч №#{rand(777)}" if self.name.blank?
   end
 
+  def link_subscriptions
+    Subscription.where(user_id: nil, user_email: self.email)
+      .update_all(user_id: self.id)
+  end
 end
