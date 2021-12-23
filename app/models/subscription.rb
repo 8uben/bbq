f9f8e1852852096d5f8d229389d6184e3207a92f
Vladislav Fedyaev <vladislav.fdv@mail.ru>
2021-12-24 01:21:29 +0300

    anonymous user can subscribe only by not busy email

--- app/models/subscription.rb
+++ app/models/subscription.rb
@@ -1,12 +1,15 @@
 class Subscription < ApplicationRecord
   belongs_to :event
-  belongs_to :user
+  belongs_to :user, optional: true
 
   # Проверки user_name и user_email выполняются,
   # только если user не задан
   # То есть для анонимных пользователей
   validates :user_name, presence: true, unless: -> { user.present? }
-  validates :user_email, presence: true, format: /\A[a-zA-Z0-9\-_.]+@[a-zA-Z0-9\-_.]+\z/, unless: -> { user.present? }
+  validates :user_email, presence: true,
+    format: /\A[a-zA-Z0-9\-_.]+@[a-zA-Z0-9\-_.]+\z/, unless: -> { user.present? }
+
+  before_validation :check_uniqueness_email, unless: -> { user.present? }
 
   # Если есть юзер, выдаем его имя,
   # если нет – дергаем исходный метод
@@ -25,6 +28,14 @@ class Subscription < ApplicationRecord
       user.email
     else
       super
+    end
+  end
+
+  private
+
+  def check_uniqueness_email
+    if User.find_by(email: user_email)
+      errors.add(:user_email, :email_used)
     end
   end
 end
