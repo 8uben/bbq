4146c8c594831ce15bb6ae014fd577ab7fad7063
Vladislav Fedyaev <vladislav.fdv@mail.ru>
2022-01-16 00:52:58 +0300

    notice about new photo by email

--- app/mailers/event_mailer.rb
+++ app/mailers/event_mailer.rb
@@ -12,7 +12,7 @@ class EventMailer < ApplicationMailer
 
     # Берём у юзер его email
     # Subject тоже можно переносить в локали
-    mail to: event.user.email, subject: "Новая подписка на #{event.title}"
+    mail to: event.user.email, subject: "#{t('.subject')} #{event.title}"
   end
 
   # Subject can be set in your I18n file at config/locales/en.yml
@@ -24,6 +24,13 @@ class EventMailer < ApplicationMailer
     @comment = comment
     @event = event
 
-    mail to: email, subject: "Новый комментарий @ #{event.title}"
+    mail to: email, subject: "#{t('.subject')} #{event.title}"
+  end
+
+  def photo(event, photo, email)
+    @photo = photo
+    @event = event
+
+    mail to: email, subject: "#{t('.subject')} #{event.title}"
   end
 end
